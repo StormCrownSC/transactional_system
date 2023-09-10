b7a415b71d36557b0e8c8be7ddf0856af8ac63d2
Denis Sirotkin <sirotkin@denis-02.ru>
2023-09-10 22:03:29 +0300

    The main package has been created

---
+++ cmd/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+
+	"github.com/StormCrownSC/transactional_system/internal/databases"
+	"github.com/StormCrownSC/transactional_system/internal/handler"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	log.Println("Start app")
+
+	db, err := databases.ConnectDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	r := gin.Default() // Creating a Gin Router
+
+	// Passing db to route handlers via closures (closure)
+	r.POST("/invoice", func(c *gin.Context) {
+		handler.CreateInvoice(c, db)
+	})
+	r.POST("/withdraw", func(c *gin.Context) {
+		handler.WithdrawFunds(c, db)
+	})
+	r.GET("/balance", func(c *gin.Context) {
+		handler.GetClientBalance(c, db)
+	})
+
+	r.Run(":8050")
+}
