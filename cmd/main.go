ad5b0081aa9346cf93b0f54ed2b9ff258715e413
Denis Sirotkin <sirotkin@denis-02.ru>
2023-11-03 11:58:51 +0300

    update project

--- cmd/main.go
+++ cmd/main.go
@@ -2,35 +2,145 @@ package main
 
 import (
 	"Service/internal/databases"
-	"Service/internal/handler"
+	"Service/internal/structures"
+	"context"
+	"database/sql"
+	"encoding/json"
+	"fmt"
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
+	"github.com/rsocket/rsocket-go"
+	"github.com/rsocket/rsocket-go/payload"
+	"github.com/rsocket/rsocket-go/rx/flux"
+	"github.com/rsocket/rsocket-go/rx/mono"
 	"log"
+	"strconv"
+	"sync"
 )
 
 func main() {
 	log.Println("Start app")
-
 	db, err := databases.ConnectDB()
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	r := gin.Default() // Creating a Gin Router
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+	ctx, cancel := context.WithCancel(context.Background())
 
-	// We specify the path to static files and the route for servicing static files
-	r.Use(static.Serve("/", static.LocalFile("./assets", true)))
-	// Passing db to route handlers via closures (closure)
-	r.POST("/invoice", func(c *gin.Context) {
-		handler.CreateInvoice(c, db)
-	})
-	r.POST("/withdraw", func(c *gin.Context) {
-		handler.WithdrawFunds(c, db)
-	})
-	r.GET("/balance", func(c *gin.Context) {
-		handler.GetClientBalance(c, db)
-	})
+	go func() {
+		defer wg.Done()
+		r := gin.Default()
+		// We specify the path to static files and the route for servicing static files
+		r.Use(static.Serve("/", static.LocalFile("./assets", true)))
+		r.Run(":8050")
+	}()
 
-	r.Run(":8050")
+	// Rsocket
+	go func() {
+		defer wg.Done()
+		err := rsocket.Receive().OnStart(func() {
+			log.Println("Server Started")
+		}).Acceptor(func(_ context.Context, _ payload.SetupPayload, _ rsocket.CloseableRSocket) (rsocket.RSocket, error) {
+			return rsocket.NewAbstractSocket(
+				// Request-Response
+				rsocket.RequestResponse(func(c payload.Payload) mono.Mono {
+					amountStr := c.DataUTF8()
+					if amountStr == "" {
+						return mono.Error(fmt.Errorf("invalid request format"))
+					}
+					amount, err := strconv.ParseFloat(amountStr, 64)
+					if err == nil {
+						return mono.Error(fmt.Errorf("invalid request format"))
+					}
+
+					err = databases.WithdrawFunds(db, structures.TransactionRequest{Account: "12345678901234567890", Currency: "AED", Amount: amount})
+					if err != nil {
+						return mono.Error(err)
+					}
+
+					return mono.Just(payload.NewString("SUCCESS", ""))
+				}),
+
+				// Request-Stream
+				rsocket.RequestStream(func(c payload.Payload) flux.Flux {
+					balances, err := databases.GetClientBalances(db, c.DataUTF8())
+					if err != nil {
+						if err == sql.ErrNoRows {
+							return flux.Error(fmt.Errorf("balances not found"))
+						} else {
+							return flux.Error(fmt.Errorf("with get balances"))
+						}
+					}
+
+					// Return a successful response
+					return flux.Create(func(_ context.Context, s flux.Sink) {
+						for _, balance := range balances {
+							data, err := json.Marshal(balance)
+							if err != nil {
+								s.Next(payload.New(nil, nil))
+							}
+							s.Next(payload.New(data, nil))
+						}
+						s.Complete()
+					})
+				}),
+
+				// Request-Channel
+				rsocket.RequestChannel(func(c flux.Flux) flux.Flux {
+					clients := make(chan string)
+					balances := make(chan []structures.ClientBalance)
+
+					c.DoOnComplete(func() {
+						close(clients)
+					}).DoOnNext(func(msg payload.Payload) error {
+						data := msg.DataUTF8()
+						if err != nil {
+							log.Fatalln(err)
+							return nil
+						}
+						clients <- data
+						return nil
+					}).Subscribe(context.Background())
+
+					go func() {
+						for client := range clients {
+							balanceStruct, _ := databases.GetClientBalances(db, client)
+							balances <- balanceStruct
+						}
+						close(balances)
+					}()
+					return flux.Create(func(_ context.Context, s flux.Sink) {
+						for balance := range balances {
+							data, err := json.Marshal(balance)
+							if err != nil {
+								s.Next(payload.New(nil, nil))
+							}
+							s.Next(payload.New(data, nil))
+						}
+						s.Complete()
+					})
+				}),
+
+				// Fire-and-forget
+				rsocket.FireAndForget(func(c payload.Payload) {
+					amountStr := c.DataUTF8()
+					if amountStr != "" {
+						amount, err := strconv.ParseFloat(amountStr, 64)
+						if err == nil {
+							databases.CreateInvoice(db, structures.TransactionRequest{Account: "12345678901234567890", Currency: "AED", Amount: amount})
+						}
+					}
+				}),
+			), nil
+		}).Transport(rsocket.TCPServer().SetAddr(":8060").Build()).Serve(ctx)
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}()
+	wg.Wait()
+	cancel()
 }
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/gin-gonic/contrib v0.0.0-20221130124618-7e01895a63f2
 	github.com/gin-gonic/gin v1.9.1
 	github.com/lib/pq v1.10.9
+	github.com/rsocket/rsocket-go v0.8.12
 )
 
 require (
@@ -18,6 +19,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/jjeffcaii/reactor-go v0.5.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
 	github.com/kr/pretty v0.3.0 // indirect
@@ -25,10 +27,13 @@ require (
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/panjf2000/ants/v2 v2.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rogpeppe/go-internal v1.11.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
 	golang.org/x/arch v0.3.0 // indirect
 	golang.org/x/crypto v0.9.0 // indirect
 	golang.org/x/net v0.10.0 // indirect
